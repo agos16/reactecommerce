bd65edd16c705567a9a630af1fcdde04d53c060c
agos16 <agoscabello2@gmail.com>
2023-10-26 20:02:04 -0300

    primer pre-entrega-gonzalez cabello

---
+++ src/components/CartWidget.js
@@ -0,0 +1,9 @@
+import cart from '../assets/cart.png'
+
+export const CartWidget= () => {
+    return(
+        <>
+        <img src={cart} alt='changuito'/>
+        <span>3</span></>
+    )
+}
---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import { CartWidget } from "./CartWidget"
+
+export const NavBar = () => {
+    return <header> 
+        <nav>
+            <div>serro revestimiento</div>
+        <ul>
+            <li>
+            <a href="">revestimiento pisos</a>
+            </li>
+            <li>
+            <a href="">revestimiento paredes</a>
+            </li>
+        </ul>
+        </nav> 
+        <CartWidget/> 
+        </header>
+}
