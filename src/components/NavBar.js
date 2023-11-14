927f5a5d3ca6053e7964bd1bc60ef898daf4d863
agos16 <agoscabello2@gmail.com>
2023-11-14 00:14:28 -0300

    Segunda entrega

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,19 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 import { CartWidget } from "./CartWidget"
 
 export const NavBar = () => {
-    return <header> 
-        <nav>
-            <div>serro revestimiento</div>
-        <ul>
-            <li>
-            <a href="">revestimiento pisos</a>
-            </li>
-            <li>
-            <a href="">revestimiento paredes</a>
-            </li>
-        </ul>
-        </nav> 
-        <CartWidget/> 
-        </header>
+    return ( <Navbar bg="dark" data-bs-theme="dark">
+    <Container>
+    <Navbar.Brand href="#home">Serro revestimiento</Navbar.Brand>
+    <Nav className="me-auto">
+        <Nav.Link href="#home">pisos</Nav.Link>
+        <Nav.Link href="#features">paredes</Nav.Link>
+        <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav><CartWidget/> 
+    </Container>
+</Navbar>
+    )
 }
