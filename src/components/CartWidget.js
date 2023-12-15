09d961a3c2732eaa42872ec699aaa7baf71d25ef
agos16 <agoscabello2@gmail.com>
2023-12-14 23:55:08 -0300

    entrega final-gonzalezcabello

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,9 +1,24 @@
-import cart from '../assets/cart.png'
+import { Link } from "react-router-dom"
+import cart from "../assets/cart.png"
+import { useContext } from "react";
+import { CartContext } from "../contexts/CartContext";
 
-export const CartWidget= () => {
-    return(
-        <>
-        <img src={cart} alt='changuito'/>
-        <span>3</span></>
-    )
-}
+
+export const CartWidget = () => {
+const { items } = useContext(CartContext);
+
+
+const total = items.reduce(
+    (acumulador, valorActual) => acumulador + valorActual.quantity,
+    0
+);
+
+return (
+    <>
+    <Link to ="/cart">
+        <img src ={cart} alt= "carrito" width = {30} />
+        <span>{total}</span>
+    </Link>
+    </>
+);
+};
--- src/components/ItemCounter.js
+++ src/components/ItemCounter.js
@@ -1,8 +1,45 @@
-export const ItemCounter = ({onAdd}) => {
-    return (
-        <>
-        <div>-</div>
-        <input />
-        <div>+</div>
-        <button onClick = {onAdd}>agregar al carrito</button></>)
-}
+import { useState } from "react";
+import { Button, Form, InputGroup } from "react-bootstrap";
+
+export const ItemCounter = ({ onAdd, stock, initial }) => {
+const [count, setCount] = useState(initial);
+
+const IncreaseCount = () => {
+    if (stock > count) {
+    setCount((prev) => {
+        onAdd(prev + 1)
+        return prev + 1
+    });
+    }
+};
+
+const DecreaseCount = () => {
+    if (count > 1) {
+    setCount((prev) => {
+        onAdd(prev - 1)
+        return prev - 1
+    });
+    }
+};
+
+
+return (
+    <>
+    <br />
+    <InputGroup className="mb-3" style={{ maxWidth: "110px" }}>
+        <Button variant="primary" id="button-addon1" onClick={DecreaseCount}>
+        -
+        </Button>
+        <Form.Control
+        aria-label="Example text with button addon"
+        aria-describedby="basic-addon1"
+        value={count}
+        readOnly
+        />
+        <Button variant="primary" id="button-addon2" onClick={IncreaseCount}>
+        +
+        </Button>
+    </InputGroup>
+    </>
+);
+};
